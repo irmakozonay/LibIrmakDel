2824dd944ff37ea61f86d7c6e83d6501535bd954
irmakozonay <irmakoz@msn.com>
2021-03-19 20:44:48 +0300

    first commit

---
+++ LibIrmakDel/LibIrmakDel.h
@@ -0,0 +1,18 @@
+//
+//  LibIrmakDel.h
+//  LibIrmakDel
+//
+//  Created by Irmak Ozonay on 19.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LibIrmakDel.
+FOUNDATION_EXPORT double LibIrmakDelVersionNumber;
+
+//! Project version string for LibIrmakDel.
+FOUNDATION_EXPORT const unsigned char LibIrmakDelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibIrmakDel/PublicHeader.h>
+
+
---
+++ LibIrmakDel/LibIrmakDel.swift
@@ -0,0 +1,18 @@
+//
+//  LibIrmakDel.swift
+//  LibIrmakDel
+//
+//  Created by Irmak Ozonay on 19.03.2021.
+//
+
+import Foundation
+
+public class LibIrmakDel : NSObject {
+    
+    public static let shared = LibIrmakDel()
+    
+    public func printLog() {
+        print("printLog")
+    }
+
+}
